54ac39913d6ededbc8379cc251c2bc823ac2abac
Shine <abhijit.shine@hotmail.com>
2019-04-23 00:07:34 -0400

    voice properties

--- src/client/constants/voiceProperties.js
+++ src/client/constants/voiceProperties.js
@@ -1,4 +1,4 @@
 export const HOTWORD = 'demo'
-export const HEROKU_VOICE_API_NAME = 'YOUR API NAME HERE'
-export const SNIPS_ACCOUNT_NAME = 'YOUR ACCOUNT NAME HERE'
-export const SNIPS_INTENTS = ['Navigate']
+export const HEROKU_VOICE_API_NAME = 'Planulife'
+export const SNIPS_ACCOUNT_NAME = 'snips-T9S648M5bU'
+export const SNIPS_INTENTS = ['findHouse']
